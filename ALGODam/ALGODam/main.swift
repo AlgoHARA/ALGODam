92cc43f563e590020ee363643ad07dbe6a6e18b3
daminoworld <ekadls757@gmail.com>
2023-03-23 22:29:46 +0900

    del: reset main.swift

--- ALGODam/ALGODam/main.swift
+++ ALGODam/ALGODam/main.swift
@@ -5,9 +5,4 @@
 //  Created by 김담인 on 2023/03/23.
 //
 
-// bj2961
-import Foundation
 
-var dic = [1:"a", 2:"b", 3:"c"]
-let sort = dic.sorted(by: {$0.key > $1.key }) // value로 sort할 경우 $0.value
-print(sort)
