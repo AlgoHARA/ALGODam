c49ae18f9e0a35e5c6f876021a6ebfd5a3c94e08
daminoworld <ekadls757@gmail.com>
2023-06-02 13:58:35 +0900

    Add: Queue with Swift

--- ALGODam/ALGODam/DataStructure/Queue.swift
+++ ALGODam/ALGODam/DataStructure/Queue.swift
@@ -1,8 +1,17 @@
-//
-//  Queue.swift
-//  ALGODam
-//
-//  Created by 김담인 on 2023/06/01.
-//
-
-import Foundation
+struct Queue<T> {
+    var enqueue: [T]
+    var dequeue: [T] = []
+    mutating func push(_ x: T) {
+        enqueue.append(x)
+    }
+    
+    mutating func pop() -> T? {
+        if dequeue.isEmpty {
+            dequeue = enqueue.reversed()
+            enqueue.removeAll()
+        }
+        
+        // popLast 자체가 옵셔널 리턴값을 가지므로 pop함수도 T? 옵셔널 리턴
+        return dequeue.popLast()
+    }
+}
